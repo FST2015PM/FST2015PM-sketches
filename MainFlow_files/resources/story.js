50ea0071e3aaf78b6120c1d973645ce07aa04154
Hasdai Pacheco <haxdai@gmail.com>
2016-10-25 21:55:11 -0500

    Initial commit

---
+++ MainFlow_files/resources/story.js
@@ -0,0 +1,106 @@
+var story = {
+ "pages": [
+  {
+   "image": "login.png",
+   "width": 900,
+   "links": [
+    {
+     "rect": [
+      451,
+      531,
+      545,
+      548
+     ],
+     "page": 1
+    },
+    {
+     "rect": [
+      301,
+      480,
+      597,
+      514
+     ],
+     "page": 3
+    }
+   ],
+   "title": "login",
+   "height": 800
+  },
+  {
+   "image": "register.png",
+   "width": 900,
+   "links": [{
+    "rect": [
+     451,
+     547,
+     561,
+     564
+    ],
+    "page": 0
+   }],
+   "title": "register",
+   "height": 800
+  },
+  {
+   "image": "admin.png",
+   "width": 900,
+   "links": [
+    {
+     "rect": [
+      855,
+      64,
+      887,
+      96
+     ],
+     "page": 0
+    },
+    {
+     "rect": [
+      814,
+      64,
+      846,
+      96
+     ],
+     "page": 3
+    }
+   ],
+   "title": "admin",
+   "height": 800
+  },
+  {
+   "image": "dashboard.png",
+   "width": 900,
+   "links": [
+    {
+     "rect": [
+      855,
+      64,
+      887,
+      96
+     ],
+     "page": 0
+    },
+    {
+     "rect": [
+      814,
+      64,
+      846,
+      96
+     ],
+     "page": 2
+    }
+   ],
+   "title": "dashboard",
+   "height": 800
+  },
+  {
+   "image": "Home.png",
+   "width": 900,
+   "links": [],
+   "title": "Home",
+   "height": 800
+  }
+ ],
+ "title": "MainFlow",
+ "highlightLinks": true
+}
